9dea517fbe838d0e38b132d1096436e35b014c91
Thomas Frössman <thomasf@jossystem.se>
2015-11-02 00:19:26 +0100

    Add command line option to test source/regexp
    Special case, not used when running as a drone plugin

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
-	"github.com/thomasf/drone-mvn/mavendeploy"
+	"flag"
+	"os"
+
 	"github.com/drone/drone-plugin-go/plugin"
+	"github.com/thomasf/drone-mvn/mavendeploy"
 )
 
+// testExpressions allows for quickly testing source/regexp patterns against
+// files via the command line.
+func testExpressions() {
+	var regexp, source string
+	flag.StringVar(&regexp, "regexp", "", "regular expression to test")
+	flag.StringVar(&source, "source", "", "source expression to test")
+	flag.Parse()
+	if regexp != "" && source != "" {
+		mvn := mavendeploy.Maven{
+			Artifact: mavendeploy.Artifact{
+				GroupID:    "GROUPID",
+				ArtifactID: "ARTIFACTID",
+				Version:    "99.99.99",
+				Extension:  "EXTENSION",
+			},
+			Args: mavendeploy.Args{
+				Debug:  true,
+				Source: source,
+				Regexp: regexp,
+			},
+		}
+		mvn.WorkspacePath(".")
+		err := mvn.Prepare()
+		if err != nil {
+			panic(err)
+		}
+		os.Exit(0)
+	}
+}
+
 func main() {
+	testExpressions()
 	workspace := plugin.Workspace{}
 	repo := plugin.Repo{}
 	build := plugin.Build{}
